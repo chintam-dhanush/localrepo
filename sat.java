e59d52f3e40bce55a5239e27fe91ead3fd7f1dd2
chintam-dhanush <chintamdhanush24@gmail.com>
2026-02-20 17:42:23 +0530

    i am back

--- sat.java
+++ sat.java
@@ -3,6 +3,7 @@ public class sat {
     public static void main(String[] args) {
         System.out.println("hi i am satwick");
         System.out.println("i am doing great");
+        hi
     }
     
 }
